1e25670d3b1425ccdc80e012411bbf1ff5e873aa
Alfonso Wenk <alfonso.wenk@esolutions.com.ar>
2024-04-06 15:19:26 -0300

    primer commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
+import React, { useState } from 'react';
+import { Error404 } from "./pages/Error404";
+import "bootstrap/dist/css/bootstrap.min.css";
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    
+    <Error404/>
     </>
   )
 }
