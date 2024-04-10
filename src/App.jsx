ada078b5965b178af82a2a5bad8744b93ba37548
Alfonso Wenk <alfonso.wenk@esolutions.com.ar>
2024-04-10 15:28:28 -0300

    primer commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { DetallePlan } from "./pages/DetallePlan"; 
+import { Contacto } from "./pages/Contacto"; 
+import "bootstrap/dist/css/bootstrap.min.css";
 
-
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-    
-    </>
-  )
-}
+    <Router>
+      <Routes>
+        <Route path="/" element={<DetallePlan />} />
+        <Route path="/contacto" element={<Contacto />} />
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
