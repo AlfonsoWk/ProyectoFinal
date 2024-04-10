733d8e628198c8c730bc7884562ef1244f3c99bb
Alfonso Wenk <alfonso.wenk@esolutions.com.ar>
2024-04-10 18:37:44 -0300

    administracion de clases y usuarios

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
+import { useState } from "react";
+import { AdminPage } from "./pages/AdminPage.jsx";
+import { UserPage } from "./pages/UserPage.jsx";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-    
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/administrador" element={<AdminPage />} />
+        <Route path="/usuarios" element={<UserPage />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
