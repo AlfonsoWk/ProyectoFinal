6d0c7a6f19b7212c7d98a3180e179cc530294bfc
Alfonso Wenk <alfonso.wenk@esolutions.com.ar>
2024-04-12 13:24:42 -0300

    se suben cambios en el package.json y App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,43 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+//import { useState } from "react";
+
+//import Principal from "./components/Principal"
+//import Registration from "./components/Registration"
+//import Login from "./components/Login"
+//import DashboardUser from "./components/DashboardUser"
+
+//import { DetallePlan } from "./pages/DetallePlan"; 
+//import { Contacto } from "./pages/Contacto"; 
+
+//import { Error404 } from "./pages/Error404";
+
+//import { Footer } from "./components/Footer";
+//import NavBar from "../components/NavBar";
+//import { AdminPage } from "./pages/AdminPage.jsx";
+//import { UserPage } from "./pages/UserPage.jsx";
+
+import { Principal } from "./pages/PaginaPrincipal.jsx";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css'
-import Principal from "./components/Principal"
-import Registration from "./components/Registration"
-import Login from "./components/Login"
-import DashboardUser from "./components/DashboardUser"
-
-
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { DetallePlan } from "./pages/DetallePlan"; 
-import { Contacto } from "./pages/Contacto"; 
-import "bootstrap/dist/css/bootstrap.min.css";
-
-const App = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<DetallePlan />} />
-        <Route path="/contacto" element={<Contacto />} />
-      </Routes>
-    </Router>
-  );
-};
-
-import React, { useState } from 'react';
-import { Error404 } from "./pages/Error404";
-import "bootstrap/dist/css/bootstrap.min.css";
-
-
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-
-import React, { useState } from 'react';
-import { Footer } from "./components/Footer";
-import "bootstrap/dist/css/bootstrap.min.css";
-import NavBar from "../components/NavBar";
-import { useState } from "react";
-import { AdminPage } from "./pages/AdminPage.jsx";
-import { UserPage } from "./pages/UserPage.jsx";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   /* const [count, setCount,] = useState(0) */
 
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
         <Route path="/" element={<Principal />} />
         <Route path="/registration" element={<Registration />} />
         <Route path="/login" element={<Login />} />
         <Route path="/dashboarduser" element={<DashboardUser />} />
-
-        
+        <Route path="/" element={<DetallePlan />} />
+        <Route path="/contacto" element={<Contacto />} />
       
       </Routes>
-    </BrowserRouter>
-  )
+    </Router>
+  );
 }
-
 
 export default App;
