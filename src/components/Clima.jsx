22b9e0f37a1748fe7c0c49f9be67721606be48bb
Alfonso Wenk <alfonso.wenk@esolutions.com.ar>
2024-06-02 16:40:22 -0300

    se actualiza API de clima

--- src/components/Clima.jsx
+++ src/components/Clima.jsx
@@ -1,24 +1,45 @@
+import React, { useEffect, useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
-
 import "../css/Clima.css";
-
 import consultarClima from "../helpers/consultarClima";
 
 const Clima = () => {
+  const [ubicacion, setUbicacion] = useState(null);
+
+  useEffect(() => {
+    obtenerUbicacion();
+  }, []);
+
+  const obtenerUbicacion = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const { latitude, longitude } = position.coords;
+          setUbicacion({ lat: latitude, lon: longitude });
+          consultarClima({ lat: latitude, lon: longitude });
+        },
+        (error) => {
+          console.error("Error al obtener la ubicación:", error);
+        }
+      );
+    } else {
+      console.error("La geolocalización no está soportada por este navegador.");
+    }
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    consultarClima();
+    consultarClima(ubicacion);
   };
 
   return (
     <>
-      <Form onSubmit={handleSubmit}>
+  {/*     <Form onSubmit={handleSubmit}>
         <Button variant="outline-light" type="submit">
           Consultar clima
         </Button>
-      </Form>
-
+      </Form> */}
       <div id="infoClima"></div>
     </>
   );
--- src/helpers/consultarClima.jsx
+++ src/helpers/consultarClima.jsx
@@ -1,28 +1,52 @@
-const consultarClima = () => {
+const traducirDescripcion = (descripcion) => {
+    const traducciones = {
+        "clear sky": "Cielo Despejado",
+        "few clouds": "Pocas Nubes",
+        "scattered clouds": "Nubes Dispersas",
+        "broken clouds": "Nubes Rotas",
+        "overcast clouds": "Nublado",
+        "shower rain": "Lluvia Ligera",
+        "rain": "Lluvia",
+        "thunderstorm": "Tormenta Eléctrica",
+        "snow": "Nieve",
+        "mist": "Neblina"
+        // Puedes agregar más traducciones según tus necesidades
+    };
+
+    // Verificar si la descripción existe en el mapeo, si no, retornar la descripción original
+    return traducciones[descripcion.toLowerCase()] || descripcion;
+};
+
+const consultarClima = (ubicacion) => {
     let ciudad = "";
     let temperatura = "";
     let icono = "";
     let logo = "";
     const infoClima = document.getElementById("infoClima");
-    const api_key = "aea1bdbb48b844228895a431240a7d11";
-    const lat = "-26.81371001999747";
-    const lon = "-65.25177608408183";
-    const url = `https://api.weatherbit.io/v2.0/current?lat=${lat}&lon=${lon}&key=${api_key}&lang=es`;
+    const api_key = "34765feb8963fd6cd54f08c7797249bd";
+    const { lat, lon } = ubicacion;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${api_key}&units=metric`;
     fetch(url)
         .then((res) => res.json())
-        .then((res2) => {
-            console.log(res2.data);
-            ciudad = res2.data[0].city_name;
-            temperatura = res2.data[0].temp;
-            icono = res2.data[0].weather.icon;
-            logo = `../src/icons/${icono}.png`;
-            const descripcion = res2.data[0].weather.description;
+        .then((data) => {
+            console.log(data);
+            ciudad = data.name;
+            temperatura = data.main.temp;
+            icono = data.weather[0].icon;
+
+            // Ajustar el formato del icono
+            const letra = obtenerLetraIcono(data.weather[0].description);
+            const numero = icono.substring(0, 2);
+            const momento = icono.charAt(2) === 'd' ? 'd' : 'n'; // 'd' para día, 'n' para noche
+            const iconoFormateado = `${letra}${numero}${momento}`;
+            logo = `../src/icons/${iconoFormateado}.png`;
+            console.log('logo: ',logo);
+
+            const descripcion = traducirDescripcion(data.weather[0].description);
             infoClima.innerHTML = `
                 <h2 class="text-center">${ciudad}</h2>
                 <h1 class="text-center">${temperatura} °C</h1>
                 <h2 class="text-center">${descripcion}</h2>
-              
-             
                 <img src="${logo}" alt="Icono del clima">
             `;
         })
@@ -31,4 +55,21 @@ const consultarClima = () => {
             infoClima.innerHTML = `<p>Error al consultar el clima. Inténtalo de nuevo más tarde.</p>`;
         });
 };
+
+const obtenerLetraIcono = (descripcion) => {
+    const letras = {
+        "clear sky": "c",
+        "few clouds": "a",
+        "scattered clouds": "a",
+        "broken clouds": "a",
+        "overcast clouds": "a",
+        "shower rain": "r",
+        "rain": "r",
+        "thunderstorm": "t",
+        "snow": "s",
+        "mist": "m"
+    };
+    return letras[descripcion.toLowerCase()] || 'a'; // Por defecto, 'a' si no se encuentra un tipo de clima específico
+};
+
 export default consultarClima;
